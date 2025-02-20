c036c8f7dbe32e144edbbd5b4ebe9b7ffdbc794c
joel harawa <joel.harawa@gmail.com>
2025-02-19 22:26:19 -0500

    setting up splash page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    const prepare = async () => {
+      // Keep the splash screen visible while resources are being loaded 
+
+      setTimeout(() => {
+        // Hide splash screen after a delay
+        SplashScreen.hideAsync();
+      }, 2000); // Adjust delay as needed
+    };
+    prepare();
+  }, []);
+
   return <Stack />;
 }
